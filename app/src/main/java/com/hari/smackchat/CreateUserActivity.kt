ab87107d34d6a39d4ea9ef3233ec090d9e9524bf
hari <krishnadude98@gmail.com>
2020-04-07 18:33:36 +0530

    lession4

---
+++ app/src/main/java/com/hari/smackchat/CreateUserActivity.kt
@@ -0,0 +1,23 @@
+package com.hari.smackchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+    fun GenerateUserAvaatar(view:View){
+
+    }
+    fun generateColourClicked(view: View){
+
+    }
+    fun crateUserClicked(view: View){
+
+    }
+
+}
